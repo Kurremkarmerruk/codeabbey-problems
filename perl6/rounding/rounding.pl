31b9b011f5016954c4f0f676300ca0207c492cd7
Jacob Russo <madcap.russo@gmail.com>
2015-02-23 23:49:51 -0600

    adjust some brackets and whitespace

--- perl6/rounding/rounding.pl
+++ perl6/rounding/rounding.pl
@@ -19,16 +19,16 @@ multi sub infix:<҂>(PositiveInt:D $n, PositiveInt:D $d) {
 }
 multi sub infix:<҂>(NegativeInt:D $n, NegativeInt:D $d) {
     given $n % $d {
-        when $_ >  $d div 2 { ($n / $d).floor }
+        when $_ >  $d div 2 { ($n / $d).floor   }
         when $_ == $d div 2 { ($n / $d).ceiling }
-        when $_ <  $d div 2 { ($n / $d).ceiling   }
+        when $_ <  $d div 2 { ($n / $d).ceiling }
     }
 }
 multi sub infix:<҂>(Int:D $n, Int:D $d) {
     given $n % $d {
-        when $_ >  $d div 2 { ($n / $d).floor }
+        when $_ >  $d div 2 { ($n / $d).floor   }
         when $_ == $d div 2 { ($n / $d).ceiling }
-        when $_ <  $d div 2 { ($n / $d).floor }
+        when $_ <  $d div 2 { ($n / $d).floor   }
     }
 }
 
